88dcc9ab8e194dd5f85295ad18312902f00649bb
Jessica Bathel <811@holbertonschool.com>
2020-01-09 23:41:52 +0000

    Version 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #Script that sets up your web servers for the deployment of web_static
 
-apt-get -y update
-install -y install nginx
-ufw allow 'Ngingx HTTP'
-mkdir -p /data/web_static/{releases/test, shared}
-cat > /data/web_static/test/index.html << 'EOF'
+apt-get update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
+mkdir -p /data/web_static/{releases/test,shared}
+cat > /data/web_static/releases/test/index.html << 'EOF'
 <html>
 	<head>
 	</head>
@@ -16,5 +16,5 @@ cat > /data/web_static/test/index.html << 'EOF'
 EOF
 ln -s /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server /a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sitesavailable/default
+sed -i '/listen 80 default_server /a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
