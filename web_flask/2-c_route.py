a373f4dae1db18933f378213a3006a2ad8e701fe
Jessica Bathel <811@holbertonschool.com>
2020-01-20 17:36:27 +0000

    Version 1

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from flask import Flask, escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    text = text.replace("_", " ")
+    return 'C %s' % escape(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    strict_slashes = False
