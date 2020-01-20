eeee9aa2728465cc211103cd05eccfae9074ae49
Jessica Bathel <811@holbertonschool.com>
2020-01-20 21:18:02 +0000

    Added default route def

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -20,8 +20,9 @@ def c(text):
     return 'C %s' % (text)
 
 
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
+def python(text='is cool'):
     text = text.replace("_", " ")
     return 'Python %s' % (text)
 
