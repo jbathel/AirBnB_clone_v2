83e68cfc02f9192537ad74147bd8f75dadea9009
Jessica Bathel <811@holbertonschool.com>
2020-01-22 22:23:12 +0000

    Removed extra imports

--- console.py
+++ console.py
@@ -2,14 +2,6 @@
 """This is the console for AirBnB"""
 import cmd
 from models import storage
-from datetime import datetime
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 from shlex import split
 
 
